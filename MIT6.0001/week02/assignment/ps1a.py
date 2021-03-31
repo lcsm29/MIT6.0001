0b41cc42bad5ac60f9534106ec2635d54a509e40
learning-curve-since-mar-29-2021 <learningcurve.since.mar.29.2021@outlook.com>
2021-03-31 12:33:35 +0900

    mostly ps1c.py

--- MIT6.0001/week02/assignment/ps1a.py
+++ MIT6.0001/week02/assignment/ps1a.py
@@ -68,9 +68,10 @@ decide that you want to start saving to buy a house. As housing prices are very 
 you realize you are going to have to save for several years before you can afford to make the down
 payment on a house. In Part A, we are going to determine how long it will take you to save enough
 money to make the down payment given the following assumptions:
+
 1. Call the cost of your dream home ​ total_cost​ .
-2. Call the portion of the cost needed for a down payment ​ portion_down_payment​ . For
-simplicity, assume that portion_down_payment = 0.25 (25%).
+2. Call the portion of the cost needed for a down payment ​ portion_down_payment​ . For simplicity,
+assume that portion_down_payment = 0.25 (25%).
 3. Call the amount that you have saved thus far ​ current_savings​ . You start with a current
 savings of $0.
 4. Assume that you invest your current savings wisely, with an annual return of ​ r ​ (in other words,
@@ -83,12 +84,15 @@ the down payment. Call that ​ portion_saved​ . This variable should be in de
 for 10%).
 7. At the end of each month, your savings will be increased by the return on your investment,
 plus a percentage of your ​ monthly salary ​ (annual salary / 12).
+
 Write a program to calculate how many months it will take you to save up enough money for a down
 payment. You will want your main variables to be floats, so you should cast user inputs to floats.
-1Your program should ask the user to enter the following variables:
+
+Your program should ask the user to enter the following variables:
 1. The starting annual salary (annual_salary)
 2. The portion of salary to be saved (portion_saved)
 3. The cost of your dream home (total_cost)
+
 Hints
 To help you get started, here is a rough outline of the stages you should probably follow in writing your
 code:
@@ -99,6 +103,7 @@ an int)
 values that represent annual amounts and those that represent monthly amounts.
 Try different inputs and see how long it takes to save for a down payment. ​ Please make your
 program print results in the format shown in the test cases below.
+
 Test Case 1
 >>>
 Enter your annual salary:​ 120000
@@ -106,6 +111,7 @@ Enter the percent of your salary to save, as a decimal: ​ . 10
 Enter the cost of your dream home:​ 1000000
 Number of months:​ 183
 >>>
+
 Test Case 2
 >>>
 Enter your annual salary:​ 80000
--- MIT6.0001/week02/assignment/ps1b.py
+++ MIT6.0001/week02/assignment/ps1b.py
@@ -75,30 +75,34 @@ Background
 In Part A, we unrealistically assumed that your salary didn’t change. But you are an MIT graduate, and
 clearly you are going to be worth more to your company over time! So we are going to build on your
 solution to Part A by factoring in a raise every six months.
+
 In ​ ps1b.py​ , copy your solution to Part A (as we are going to reuse much of that machinery). Modify
 your program to include the following
+
 1. Have the user input a semi-annual salary raise ​ semi_annual_raise​ (as a decimal percentage)
 2. After the 6​ th​ month, increase your salary by that percentage. Do the same after the 12 th
 month, the 18​ th ​ month, and so on.
+
 Write a program to calculate how many months it will take you save up enough money for a down
 payment. LIke before, assume that your investments earn a return of ​ r ​ = 0.04 (or 4%) and the
 required down payment percentage is 0.25 (or 25%). Have the user enter the following variables:
+
 1. The starting annual salary (annual_salary)
-22. The percentage of salary to be saved (portion_saved)
+2. The percentage of salary to be saved (portion_saved)
 3. The cost of your dream home (total_cost)
 4. The semi­annual salary raise (semi_annual_raise)
+
 Hints
-To help you get started, here is a rough outline of the stages you should probably follow in writing your
+To help you get started, here is a rough outline of the stages you should probably follow in writing your 
 code:
 ● Retrieve user input.
 ● Initialize some state variables. You should decide what information you need. Be sure to be
 careful about values that represent annual amounts and those that represent monthly amounts.
 ● Be careful about when you increase your salary – this should only happen ​ after​ the 6​ th​ , 12 th
-​ , 18 th
-month, and so on.
-Try different inputs and see how quickly or slowly you can save enough for a down payment. P
-lease
+​ , 18 th month, and so on.
+Try different inputs and see how quickly or slowly you can save enough for a down payment. Please
 make your program print results in the format shown in the test cases below.
+
 Test Case 1
 >>>
 Enter your starting annual salary:​ 120000
@@ -107,6 +111,7 @@ Enter the cost of your dream home: ​ 500000
 Enter the semi­annual raise, as a decimal:​ .03
 Number of months:​ 142
 >>>
+
 Test Case 2
 >>>
 Enter your starting annual salary:​ 80000
@@ -115,6 +120,7 @@ Enter the cost of your dream home: ​ 800000
 Enter the semi­annual raise, as a decimal:​ .03
 Number of months:​ 159
 >>>
+
 Test Case 3
 >>>
 Enter your starting annual salary:​ 75000
