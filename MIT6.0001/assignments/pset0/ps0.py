61e875c9cd1eaedfa9b5061af1f72269f47e16cd
learningcurve-since-mar-29-2021 <learningcurve.since.mar.29.2021@outlook.com>
2021-04-08 09:04:06 +0900

    removed the comments that contains the problem set

--- MIT6.0001/assignments/pset0/ps0.py
+++ MIT6.0001/assignments/pset0/ps0.py
@@ -4,20 +4,3 @@ x=float(input("Enter number x:"))
 y=float(input("Enter number y:"))
 print("x**y =", x**y)
 print("log(x) =", math.log(x,2))
-
-'''
-Assignment:
-Write a program that does the following in order:
-1. Asks the user to enter a number “x”
-2. Asks the user to enter a number “y” 
-3. Prints out number “x”, raised to the power “y”.
-4. Prints out the log (base 2) of “x”. 
-Use Spyder to create your program, and save your code in a file named ‘ps0.py’. An example 
-of an interaction with your program is shown below. The words printed in blue are ones the 
-computer should print, based on your commands, while the words in black are an example of 
-a user's input. The colors are simply here to help you distinguish the two components.
-Enter number x: 2
-Enter number y: 3
-X**y = 8
-log(x) = 1
-'''
